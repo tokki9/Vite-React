63b7706882279630dd9d6af8f9a254d69eb1c3db
tokki9 <tokkikun59@gmail.com>
2024-09-29 22:02:25 -0500

    Uso de eventos

--- src/App.jsx
+++ src/App.jsx
@@ -9,23 +9,31 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header';
-import { List, ListItem, ListItemText } from '@mui/material';
+import { Button, Typography } from '@mui/material';
 
 function App() {
-  const tecnologias = ['React', 'Vite', 'Material UI', 'Github'];
+  const [contador, setContador] = useState(0);
+
+  const incrementar = () => {
+    setContador(contador + 1);
+  };
 
   return (
     <div>
-      <Header titulo="Tecnologías Usadas" />
-      <List>
-        {tecnologias.map((tecnologia, index) => (
-          <ListItem key={index}>
-            <ListItemText primary={tecnologia} />
-          </ListItem>
-        ))}
-      </List>
+      <Header titulo="Contador con Evento" />
+      <Typography variant="h4" component="div" sx={{ marginTop: 2 }}>
+        Contador: {contador}
+      </Typography>
+      <Button 
+        variant="contained" 
+        color="primary" 
+        onClick={incrementar} 
+        sx={{ marginTop: 2 }}
+      >
+        Incrementar
+      </Button>
     </div>
   );
 }
