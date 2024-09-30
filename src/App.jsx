696e1db23e24ec92897eb8e706159fcce4f21e97
tokki9 <tokkikun59@gmail.com>
2024-09-29 21:30:32 -0500

    Uso del componente header

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,25 @@
 Archivo : index.html
 Autor : Paolo Jesus Mansilla Ttito
 Fecha : 29/09/2024 
-Descripción : Creacion de pagina con enlace externo (No LocalHost)
+Descripción : utilizamos React Developer Tools para depurar y verificar 
+el estado y props de los componentes en tiempo real, junto con las herramientas 
+de Visual Studio Code para gestionar el flujo del código y automatizar el despliegue 
+en GitHub Pages.
 */
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './Header';
 
 function App() {
   return (
-    <div>
-      <h1>Hola, esta es mi página web React con Vite con enlace externo
-        de Github
-      </h1>
+    <div> 
+      <Header />  //Aquí estamos usando JSX para renderizar el componente Header
+      <p>Esta es la página principal.</p>  
     </div>
   );
 }
 
 export default App;
+
